887c57e717614a4ead8171a6fd7a05af0d2b7b40
Damir Dizdarevic <dizdarevic@zoho.com>
2022-03-13 18:59:25 +0100

    Permissions

--- app/src/main/java/com/dizdarevic/weatherapp/models/Weather.kt
+++ app/src/main/java/com/dizdarevic/weatherapp/models/Weather.kt
@@ -5,8 +5,8 @@ data class Weather(
     val current: Current,
     val daily: List<Daily>,
     val hourly: List<Hourly>,
-    val lat: Int,
-    val lon: Int,
+    val lat: Double,
+    val lon: Double,
     val minutely: List<Minutely>,
     val timezone: String,
     val timezone_offset: Int
--- app/src/main/java/com/dizdarevic/weatherapp/ui/fragments/MainFragment.kt
+++ app/src/main/java/com/dizdarevic/weatherapp/ui/fragments/MainFragment.kt
@@ -1,5 +1,9 @@
 package com.dizdarevic.weatherapp.ui.fragments
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,10 +11,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.dizdarevic.weatherapp.databinding.FragmentMainBinding
 import com.dizdarevic.weatherapp.models.Weather
 import com.dizdarevic.weatherapp.repository.MainRepository
 import com.dizdarevic.weatherapp.repository.RetrofitService
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -18,7 +28,7 @@ import retrofit2.Response
 class MainFragment : Fragment() {
     lateinit var binding:FragmentMainBinding
     private val TAG = "MainFragment"
-
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,25 +41,83 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Register the permissions callback, which handles the user's response to the
+        // system permissions dialog. Save the return value, an instance of
+        // ActivityResultLauncher. You can use either a val, as shown in this snippet,
+        // or a lateinit var in your onAttach() or onCreate() method.
+        val requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    loadData()
+                } else {
+                    // Explain to the user that the feature is unavailable because the
+                    // features requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                }
+            }
+
+        when {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // You can use the API that requires the permission.
+                loadData()
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                // In an educational UI, explain to the user why your app requires this
+                // permission for a specific feature to behave as expected. In this UI,
+                // include a "cancel" or "no thanks" button that allows the user to
+                // continue using your app without granting the permission.
+
+                //showInContextUI(...)
+            }
+            else -> {
+                // You can directly ask for the permission.
+                // The registered ActivityResultCallback gets the result of this request.
+                requestPermissionLauncher.launch(
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }
+
+    }
+
+    private fun loadData() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
 
         val repository = MainRepository(RetrofitService.getInstance())
 
-        val response = repository.getWeather(44.0, 20.0)
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
 
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location : Location? ->
+                location?.let {
+                    val response = repository.getWeather(it.latitude, it.latitude)
 
-        response.enqueue(object : Callback<Weather> {
-            override fun onResponse(call: Call<Weather>, response: Response<Weather>) {
-                setData(response.body())
+                    response.enqueue(object : Callback<Weather> {
+                        override fun onResponse(call: Call<Weather>, response: Response<Weather>) {
+                            setData(response.body())
+                        }
+
+                        override fun onFailure(call: Call<Weather>, t: Throwable) {
+                            Toast.makeText(requireContext(), t.message, Toast.LENGTH_SHORT).show()
+                            Log.i(TAG, "onFailure: "+t.message)
+                        }
+                    })
+                }
             }
-
-            override fun onFailure(call: Call<Weather>, t: Throwable) {
-                Toast.makeText(requireContext(), t.message, Toast.LENGTH_SHORT).show()
-                Log.i(TAG, "onFailure: "+t.message)
+            .addOnFailureListener {
+                it.printStackTrace()
             }
-        })
     }
 
     private fun setData(body: Weather?) {
-
+        binding.test.text=body.toString()
     }
 }
